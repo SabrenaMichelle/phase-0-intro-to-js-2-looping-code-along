39973b9575ab0d86ea049c8b028fcf53f5181c73
SabrenaMichelle <sabrenaray@yahoo.com>
2021-03-21 00:46:01 -0400

    Looping While and For

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const name = ['Roxie', 'Michelle', 'Ryan'];
+    
+function writeCards(name) {
+for (let i = 0; i < name.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful birthday gift!`);
+    debugger;
+}
+return name
+}
+
+writeCards(name);
+
+let countdown = 10;
+while (countdown >= 0) {
+  console.log(countdown--);
+}
